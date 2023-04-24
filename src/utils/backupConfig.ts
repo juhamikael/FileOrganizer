3de7468de067d5372f3aefe2e0cbbdc5827db657
Juhamikael <contact.juhamusic@gmail.com>
2023-04-24 03:22:23 +0300

    Delete backupConfig.ts

--- src/utils/backupConfig.ts
+++
@@ -1,6 +0,0 @@
-export const readBackupStatus = async () => {
-  const response = await fetch("config.json");
-  const config = await response.json();
-  const enableBackup: boolean = config.enableBackup;
-  return enableBackup;
-};
